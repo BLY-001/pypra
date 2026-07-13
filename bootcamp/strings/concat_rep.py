be9b0f7c057b156e6b7db14f469675c2457db99a
ybinlawa <yusufibnlawall@gmail.com>
2026-07-13 11:57:44 +0100

    conactenation and repetition of string in python

---
+++ bootcamp/strings/concat_rep.py
@@ -0,0 +1,18 @@
+movie = 'the Godfather'
+director = 'francis ford coppola'
+movie_and_director = movie + " " + director
+print(movie_and_director)
+print(movie + ' was directly')
+print(movie + 'directed by' + director)
+print(movie, director)
+
+# repetition
+print(movie * 10)
+print('#' * 10)
+
+my_str = 'Python is TIOBE language of the year'
+s1 = my_str[9]
+print(s1)
+s2 = my_str[-3]
+print(s2)
+print(s1 + s2)
