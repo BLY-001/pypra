49f166c1639676950579c8c92dad0e426b15571b
ybinlawa <yusufibnlawall@gmail.com>
2026-07-21 11:58:06 +0100

    using loop to add the sum of numbers between 1 and 25

---
+++ bootcamp/loops/code_xer.py
@@ -0,0 +1,8 @@
+my_sum = 0
+my_product = 1
+
+for n in range(1, 26):
+    my_sum += n
+    my_product *= n
+print(f"the sum of all the numbers is {my_sum}")
+print(f" the product of all the numbers is {my_product}")
